8d782c29a92a0e00dfd4b1db19a40c2465432d2b
alystxyz <alystxyz@gmail.com>
2022-12-16 14:17:58 -0500

    hackathon landing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :projects
-  resources :catalysts, controller: "campaigns"
+  # resources :projects
+  # resources :catalysts, controller: "campaigns"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
