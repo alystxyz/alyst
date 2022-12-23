ff06a4bfc81555c2a9b79544a66cdbc6a5b1b8a3
alystxyz <alystxyz@gmail.com>
2022-12-23 10:53:18 -0500

    add default_url_options for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,6 +40,7 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
+  Rails.application.routes.default_url_options[:host] = "alyst.xyz"
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
   # config.action_cable.url = "wss://example.com/cable"
